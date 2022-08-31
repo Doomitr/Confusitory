80c481d4d35c0d3f50cd1b2555eec3a4cb5d4c49
Pasedinic Dmitrii <pasecinic.dumitru@gmail.com>
2022-08-31 20:47:23 +0300

    'text'

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+K
+src/md/tekwill/pack1/A.java,f\a\fa70a661eae6b0e22d3db28d9f1a9402b2814637
+O
+src/md/tekwill/pack4/Ipod1.java,6\a\6a8e5246ed202d8fa3defb84681bcc8507f64e03
+O
+src/md/tekwill/pack5/Ipod2.java,b\6\b60f0a10d71ad0e8b5ea8f179327ed8b18a022e1
+L
+src/md/tekwill/pack1/AA.java,6\e\6e573a0969099a9740eec1e04a994f761ea1bc99
---
+++ src/md/tekwill/ui/TestEmployee.java
@@ -0,0 +1,17 @@
+package md.tekwill.ui;
+
+import md.tekwill.model.Employee;
+import md.tekwill.service.EmployeeService;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Ion", "Bostan");
+        Employee employee2 = new Employee("Vasile", "Grosu");
+        EmployeeService employeeService = new EmployeeService();
+        boolean isEmployeeSaved = employeeService.create(employee);
+       employeeService.create(employee2);
+        employeeService.listEmployees();
+//        System.out.println(isEmployeeSaved);
+
+    }
+}
